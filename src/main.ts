26e01770579cd4980150afc78fa72bbc7399247a
An Nguyen <an.nguyen@vnetwork.vn>
2025-10-02 23:15:51 +0700

    feat: enable CORS in main application setup
    - Configured CORS settings to allow all origins and specified allowed methods and headers for API requests.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+    maxAge: 86400,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
